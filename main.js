8c9717b614d1dd8debfc8a87fe4d9207cbceaabe
Dimitar Roustchev <dimitar.roustchev@stylight.com>
2013-04-20 17:35:47 +0200

    Stripped down existing chrome app.

---
+++ main.js
@@ -0,0 +1,23 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+
+  var isAliveCheck = 0,
+      appWindow = null;
+
+  isAliveCheck = setInterval(function() {
+    if(appWindow && appWindow.closed && appWindow.DRONE) {
+      appWindow.DRONE.API.shutdown();
+      appWindow=null;
+      if (isAliveCheck) clearInterval(isAliveCheck);
+    }
+  }, 1000);
+
+  chrome.app.window.create('index.html', {
+    bounds: {
+      width: 800,
+      height: 700
+    }
+  }, function(createdWindow) {
+    appWindow = createdWindow.dom;
+  });
+
+});
