5e262277bb454df078338057282e0026c65ef17e
Sergii Khomenko <sergii.khomenko@stylight.de>
2013-04-21 06:45:36 +0200

    changed queue algo

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,7 @@ var failed = false;
 
 var interval;
 var handler = "";
+var searchInterval;
 
 $(document).ready(function(){
 
@@ -12,6 +13,7 @@ $(document).ready(function(){
         setTimeout(function(){DRONE.API.shutdown();}, 1000);
         DRONE.TweetQueue.cancel();
         Notifications.notify('success','Killed connection to Drone!');
+        clearTimeout(searchInterval);
     });
 
 
@@ -34,18 +36,15 @@ $(document).ready(function(){
         chrome.storage.sync.set({'handler': handlerVal}, function(){
             // Notify about success
             Notifications.notify('success','Twitter handle saved successfully!');
-        })
+        });
     });
 
 });
 
 
 $('#toggle-settings').on('click', function(e) {
-
   $('#settings').toggle();
-
-
-})
+});
 
 function displayNavData(navdata) {
   var
@@ -104,25 +103,39 @@ function extractCommand(msg) {
 
 }
 
-function searchTwitter(query) {
+function searchTwitter(query, lastId) {
+    var batchId = 0;
     $.ajax({
         url: 'http://search.twitter.com/search.json?q=' + query,
         //dataType: 'jsonp',
         success: function(data) {
-            console.log(data['results']);
             for (var i = 0; i < data['results'].length; i++) {
                 //DRONE.TweetQueue.push(data['results'][i]['text']);
-                DRONE.TweetQueue.push({'msg': data['results'][i]['text'], 'cmd': extractCommand(data['results'][i]['text']), 'meta': data['results'][i]});
-                console.log(data['results'][i]['text']);
+                if (data['results'][i]['id'] > batchId) {
+                    batchId = parseInt(data['results'][i]['id'], 10);
+
+                }
+                if (batchId > lastId) {
+                    console.log('addind some items' + batchId + " > " + lastId);
+                    console.log(batchId - lastId);
+                    DRONE.TweetQueue.push({'msg': data['results'][i]['text'], 'cmd': extractCommand(data['results'][i]['text']), 'meta': data['results'][i]});
+                    console.log(data['results'][i]);
+                }
             }
+            setTimeout(function() {
+                searchInterval = searchTwitter(query, batchId);
+            }, 1000);
         }
     });
+
 }
 
 
 $('#start').click(function() {
     DRONE.API.init(onDroneConnected, onDroneConnectionFailed);
-    searchTwitter(handler.replace('#', '%23'));
-    //TODO:clear the queue before every start
+    clearTimeout(searchInterval);
+    DRONE.TweetQueue.clear();
+
+    searchTwitter(handler.replace('#', '%23'), 0);
 });
 
