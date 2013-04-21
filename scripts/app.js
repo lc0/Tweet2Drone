1843405caaa3941d294dd3b3dc5a60480f05509d
Dimitar Roustchev <dimitar.roustchev@stylight.com>
2013-04-21 03:36:03 +0200

    Added timeout to shutdown, so land can finish on emergency stop.

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     // Emergency stop button
     $('#cancel').click(function() {
         DRONE.API.land();
-        DRONE.API.shutdown();
+        setTimeout(function(){DRONE.API.shutdown();},1000});
         DRONE.TweetQueue.cancel();
         Notifications.notify('success','Killed connection to Drone!');
     });
